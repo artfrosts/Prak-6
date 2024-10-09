c85479159e6fc1d0bf686bf5d0a4021716d3cba0
Артем <@DESKTOP-ULJ7M14>
2024-10-09 23:49:37 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using WpfApp25.Model;
+
+namespace WpfApp25
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            DatabaseFacade facade = new DatabaseFacade(new ModelContext());
+            facade.EnsureCreated();
+        }
+    }
+
+}
